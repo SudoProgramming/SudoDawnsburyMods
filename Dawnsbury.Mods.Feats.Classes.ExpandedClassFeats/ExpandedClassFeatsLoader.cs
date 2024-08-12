6ca759a281000fd5b3206c27081a81765a9732ea
Sudo <sudoprogramming@gmail.com>
2024-08-12 14:35:17 -0500

    - Finished Rage Text Updates - Added Additional Rage damage to Fury - Removed old Dragon Insticts - Added new Dragon Insticts

--- Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats/ExpandedClassFeatsLoader.cs
+++ Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats/ExpandedClassFeatsLoader.cs
@@ -9,16 +9,22 @@ using Dawnsbury.Core.Mechanics;
 using Dawnsbury.Core.Mechanics.Core;
 using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Core.Possibilities;
+using Dawnsbury.Core.Roller;
 using Dawnsbury.Modding;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using static Dawnsbury.Core.CharacterBuilder.FeatsDb.TrueFeatDb.BarbarianFeatsDb;
 
 namespace Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats
 {
     public class ExpandedClassFeatsLoader
     {
+        private static readonly List<FeatName> originalDragonInstincts = new List<FeatName>() { FeatName.DragonInstinctFire, FeatName.DragonInstinctCold, FeatName.DragonInstinctElectricity, FeatName.DragonInstinctSonic, FeatName.DragonInstinctAcid };
+
         /// <summary>
         /// Runs on launch and patches the feats
         /// </summary>
@@ -27,6 +33,7 @@ namespace Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats
         {
             Debugger.Launch();
             PatchFeats();
+            AddFeats(CreateRemasteredBarbarianFeats());
         }
 
         /// <summary>
@@ -34,6 +41,15 @@ namespace Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats
         /// </summary>
         private static void PatchFeats()
         {
+            AllFeats.All.RemoveAll((feat) => feat is DragonInstinctFeat && originalDragonInstincts.Contains(feat.FeatName));
+            ModManager.RegisterActionOnEachActionPossibility(action =>
+            {
+                if (action.Name == "Rage")
+                {
+                    action.Description = action.Description.Replace("• You take a -1 penalty to AC.\n", string.Empty);
+                    action.ShortDescription = action.ShortDescription.Replace(" and a penalty to AC until the end of the encounter", string.Empty);
+                }
+            });
             AllFeats.All.ForEach((feat) =>
             {
                 // Updates the 'Barbarian' class feature to include the 'Quick-Tempered' feature.
@@ -53,12 +69,50 @@ namespace Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats
                             StartOfCombat = async (QEffect qEffect) =>
                             {
                                 Creature owner = qEffect.Owner;
-                                if (!owner.Armor.Item.Traits.Contains(Trait.HeavyArmor) && await owner.Battle.AskForConfirmation(owner, IllustrationName.Rage, "Enter {i}rage{/i} as a free action?", "Rage!"))
+                                if (!owner.Armor.Item.Traits.Contains(Trait.HeavyArmor) && await owner.Battle.AskForConfirmation(owner, IllustrationName.Rage, "Enter rage as a free action?", "Rage!"))
                                 {
                                     typeof(BarbarianFeatsDb).GetMethod("EnterRage", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { owner });
                                 }
                             }
                         });
+                    });
+
+                    classSelectionFeat.Subfeats.ForEach(subClass =>
+                    {
+                        if (subClass.Name == "Fury Instinct")
+                        {
+                            subClass.RulesText = "Increase the additional damage from Rage from 2 to 3. " + subClass.RulesText;
+                            var zz = subClass.RulesText;
+                            var aa = subClass;
+                            subClass.WithOnCreature(delegate (Creature cr)
+                            {
+                                cr.AddQEffect(new QEffect
+                                {
+                                    StateCheck = delegate (QEffect sc)
+                                    {
+                                        QEffect qEffect = sc.Owner.QEffects.FirstOrDefault((QEffect qfId) => qfId.Id == QEffectId.Rage);
+                                        if (qEffect != null)
+                                        {
+                                            qEffect.YouDealDamageWithStrike = null;
+                                        }
+                                    },
+                                    AddExtraStrikeDamage = delegate (CombatAction attack, Creature defender)
+                                    {
+                                        Creature owner = attack.Owner;
+                                        bool doesRageApplyToAcction = (bool)typeof(BarbarianFeatsDb).GetMethod("DoesRageApplyToAction", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { attack });
+                                        if (owner.HasEffect(QEffectId.Rage) && doesRageApplyToAcction && attack.Item != null)
+                                        {
+                                            List<DamageKind> list = attack.Item.DetermineDamageKinds();
+                                            DamageKind damageTypeToUse = defender.WeaknessAndResistance.WhatDamageKindIsBestAgainstMe(list);
+                                            DiceFormula item3 = DiceFormula.FromText((attack.HasTrait(Trait.Unarmed) || attack.HasTrait(Trait.Agile)) ? "1" : "3", (attack.HasTrait(Trait.Unarmed) || attack.HasTrait(Trait.Agile)) ? "Barbarian rage (agile)" : "Barbarian rage");
+                                            return (item3, damageTypeToUse);
+                                        }
+
+                                        return null;
+                                    }
+                                });
+                            });
+                        }
                     });
 
                     classSelectionFeat.WithPermanentQEffect(null, (QEffect qEffect) =>
@@ -159,6 +213,27 @@ namespace Dawnsbury.Mods.Feats.Classes.ExpandedClassFeats
                     //});
                 }
             });
+        }
+
+        private static void AddFeats(IEnumerable<Feat> feats)
+        {
+            foreach (Feat feat in feats)
+            {
+                ModManager.AddFeat(feat);
+            }
+        }
+
+        private static IEnumerable<Feat> CreateRemasteredBarbarianFeats()
+        {
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Adamantine dragon"), DamageKind.Bludgeoning);
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Conspirator dragon"), DamageKind.Poison);
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Diabolic dragon"), DamageKind.Fire);
+            // TODO: When spirit damage is added ad Empureal
+            //yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Empureal dragon"), DamageKind.Spirit);
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Fortune dragon"), DamageKind.Force);
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Horned dragon"), DamageKind.Poison);
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Mirage dragon"), DamageKind.Mental);
+            yield return new DragonInstinctFeat(ModManager.RegisterFeatName("Omen dragon"), DamageKind.Mental);
         }
     }
 }

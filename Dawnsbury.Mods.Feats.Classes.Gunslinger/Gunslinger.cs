6a7e0773775ee02e37eaa027c947e33694ad484c
Sudo <sudoprogramming@gmail.com>
2024-08-20 15:22:45 -0500

    Continued Work

--- Dawnsbury.Mods.Feats.Classes.Gunslinger/Gunslinger.cs
+++ Dawnsbury.Mods.Feats.Classes.Gunslinger/Gunslinger.cs
@@ -4,6 +4,8 @@ using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Modding;
 using System.Collections.Generic;
 using Dawnsbury.Mods.Items.Firearms;
+using Dawnsbury.Core.CharacterBuilder.Selections.Options;
+using Dawnsbury.Core.CharacterBuilder;
 
 namespace Dawnsbury.Mods.Feats.Classes.Gunslinger
 {
@@ -15,9 +17,12 @@ namespace Dawnsbury.Mods.Feats.Classes.Gunslinger
         /// <summary>
         /// The Registered Giant Instict Feat Name
         /// </summary>
-        public static readonly FeatName GunslingerClassFeat = ModManager.RegisterFeatName("GunslingerClassFeat", "Gunslinger");
+        public static readonly FeatName GunslingerClassFeatName = ModManager.RegisterFeatName("GunslingerClassFeat", "Gunslinger");
 
-        public static readonly FeatName WayOfTheDrifterFeat = ModManager.RegisterFeatName("Way of the Drifter", "Way of the Drifter");
+        /// <summary>
+        /// The Way of the Drifter Subclass Feat Name
+        /// </summary>
+        public static readonly FeatName WayOfTheDrifterFeatName = ModManager.RegisterFeatName("Way of the Drifter", "Way of the Drifter");
 
         /// <summary>
         /// The Gunslinger class trait
@@ -32,7 +37,7 @@ namespace Dawnsbury.Mods.Feats.Classes.Gunslinger
         /// <returns>The Enumerable of Gunslinger Feats</returns>
         public static IEnumerable<Feat> CreateGunslingerFeats()
         {
-            yield return new ClassSelectionFeat(GunslingerClassFeat, "While some fear projectile weapons, you savor the searing flash, wild kick, and cloying smoke that accompanies a gunshot, or snap of the cable and telltale thunk of your crossbow just before your bolt finds purchase. Ready to draw a bead on an enemy at every turn, you rely on your reflexes, steady hand, and knowledge of your weapons to riddle your foes with holes.",
+            yield return new ClassSelectionFeat(GunslingerClassFeatName, "While some fear projectile weapons, you savor the searing flash, wild kick, and cloying smoke that accompanies a gunshot, or snap of the cable and telltale thunk of your crossbow just before your bolt finds purchase. Ready to draw a bead on an enemy at every turn, you rely on your reflexes, steady hand, and knowledge of your weapons to riddle your foes with holes.",
                 GunslingerTrait, new EnforcedAbilityBoost(Ability.Dexterity), 8,
                 [Trait.Will, Trait.Unarmed, Trait.Simple, Trait.Martial, Firearms.AdvancedCrossbowTrait, Firearms.AdvancedFirearmTrait, Trait.UnarmoredDefense, Trait.LightArmor, Trait.MediumArmor],
                 [Trait.Perception, Trait.Fortitude, Trait.Reflex, Trait.SimpleCrossbow, Firearms.MartialCrossbowTrait, Firearms.SimpleFirearmTrait, Firearms.MartialFirearmTrait],
@@ -41,7 +46,9 @@ namespace Dawnsbury.Mods.Feats.Classes.Gunslinger
                 "{b}2. Singular Expertise{/b} You have particular expertise with guns and crossbows that grants you greater proficiency with them and the ability to deal more damage. You gain a +1 circumstance bonus to damage rolls with firearms and crossbows.\r\n\r\nThis intense focus on firearms and crossbows prevents you from reaching the same heights with other weapons. Your proficiency with unarmed attacks and with weapons other than firearms and crossbows can't be higher than trained, even if you gain an ability that would increase your proficiency in one or more other weapons to match your highest weapon proficiency (such as the weapon expertise feats many ancestries have). If you have gunslinger weapon mastery, the limit is expert, and if you have gunslinging legend, the limit is master.\n\n" +
                 "{b}3. Gunslinger Feat{/b}", new List<Feat>()
                 {
-
+                    new Feat(WayOfTheDrifterFeatName, "You're a wanderer traveling from land to land with your gun and a melee weapon as company. Maybe you learned to fight with blade and pistol as a Shackles pirate, mastered the hand cannon and katana in Minkai, or practiced with a hatchet and clan pistol in Dongun Hold. You win battles by relying on mobility and flexible use of your weapons.",
+                    "You gain the {i}Slinger's Reload, Initial Deed, and Way Skill{/i} below:\n\n" +
+                    "{b}Slinger's Reload{/b} ", new List<Trait>(), null),
                 });
         }
     }

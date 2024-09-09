ef9986c6ad3ed513b7e33d7843120d1ad381b1f1
Sudo <sudoprogramming@gmail.com>
2024-09-09 10:50:46 -0500

    Started the Ratfolk

---
+++ Dawnsbury.Mods.Feats.Ancestries.Ysoki/RatfolkLoader.cs
@@ -0,0 +1,34 @@
+﻿using Dawnsbury.Core.CharacterBuilder.Feats;
+using Dawnsbury.Modding;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Dawnsbury.Mods.Feats.Ancestries.Ratfolk
+{
+    /// <summary>
+    /// Loads the Ratfolk content
+    /// </summary>
+    public class RatfolkLoader
+    {
+        /// <summary>
+        /// Runs on launch and loads all the mod content
+        /// </summary>
+        [DawnsburyDaysModMainMethod]
+        public static void LoadMod()
+        {
+            AddFeats(Ratfolk.CreateRatfolkFeats());
+        }
+
+        /// <summary>
+        /// Adds the provided feats via the ModManager
+        /// </summary>
+        /// <param name="feats">The feats to add</param>
+        private static void AddFeats(IEnumerable<Feat> feats)
+        {
+            foreach (Feat feat in feats)
+            {
+                ModManager.AddFeat(feat);
+            }
+        }
+    }
+}
